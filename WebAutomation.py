bb9c2138efe2455feba5975f13cccf25c769d222
Kevin6525 <69578702+Kevin6525@users.noreply.github.com>
2021-01-11 12:26:38 -0800

    Update WebAutomation.py

--- WebAutomation.py
+++ WebAutomation.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import getpass
 import os.path
 PATH = "C:\Program Files (x86)\chromedriver.exe"
